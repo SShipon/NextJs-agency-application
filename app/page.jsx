5cfd3857f6b214747f54dbd99503efc71d6bd59a
Nozibul <nozibulislamspi@gmail,com>
2023-10-02 12:18:50 +0600

    api calling for brand component and configuration next.config file

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     <>
      <Hero />
      <Stats />
-     <FeatureProjects />
+     <FeatureProjects txt={true} />
      <Brands />
     </>
   )
--- components/Brands.jsx
+++ components/Brands.jsx
@@ -1,63 +1,40 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Brands = () => {
+async function getData() {
+  const data = await fetch(process.env.BASE_URL + "BrandList");
+
+  if (!data.ok) throw new Error("Brands List data calling failed");
+
+  return data.json();
+}
+
+const Brands = async () => {
+  const brandsData = await getData();
+
   return (
-    <section className="py-20">
+    <section className="py-16">
       <div className="container mx-auto px-4 text-center">
-        <h3 className="mb-16 text-2xl text-emerald-600 font-bold font-heading">
+        <h1 className="w-2/5 mx-auto rounded mb-20 text-2xl text-gray-50 bg-emerald-700 p-2 font-bold font-heading">
           Trusted by brands all over the world
-        </h3>
+        </h1>
         <div className="flex flex-wrap -mx-2">
-          <div className="mb-12 lg:mb-0 w-full md:w-1/2 lg:w-1/4 px-2">
-            <Link href="/">
-              <Image
-                className="mx-auto"
-                src="/atis-assets/logo/brands/slack.png"
-                alt=""
-                width={100}
-                height={32}
-                priority={true}
-              />
-            </Link>
-          </div>
-          <div className="mb-12 lg:mb-0 w-full md:w-1/2 lg:w-1/4 px-2">
-            <Link href="/">
-              <Image
-                className="mx-auto"
-                src="/atis-assets/logo/brands/dropbox.png"
-                alt=""
-                width={120}
-                height={32}
-                priority={true}
-                
-              />
-            </Link>
-          </div>
-          <div className="mb-12 md:mb-0 w-full md:w-1/2 lg:w-1/4 px-2">
-            <Link href="/">
-              <Image
-                className="mx-auto"
-                src="/atis-assets/logo/brands/spotify.png"
-                alt=""
-                width={100}
-                height={100}
-                priority={true}
-              />
-            </Link>
-          </div>
-          <div className="w-full md:w-1/2 lg:w-1/4 px-2">
-            <Link href="/">
-              <Image
-                className="mx-auto"
-                src="/atis-assets/logo/brands/stripe.png"
-                alt=""
-                width={70}
-                height={100}
-                priority={true}
-              />
-            </Link>
-          </div>
+          { brandsData?.length > 0
+            ? brandsData.map((item) => (
+                <div key={item.id} className="mb-12 lg:mb-0 w-full md:w-1/2 lg:w-1/4 px-2">
+                  <Link href="/">
+                    <Image
+                      className="mx-auto"
+                      src={item.image}
+                      alt={item.name}
+                      width={100}
+                      height={30}
+                      priority={true}
+                    />
+                  </Link>
+                </div>
+              ))
+            : null }
         </div>
       </div>
     </section>
